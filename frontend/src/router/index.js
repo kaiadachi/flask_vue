fa0380183216761259a6d025db549fb4cea6a0fc
kaiadachi <kaikaikai8217@gmail.com>
2018-12-11 20:02:53 +0900

    left design

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Home from '@/views/Home'
 import About from '@/views/About'
 
@@ -8,13 +7,9 @@ Vue.use(Router)
 
 export default new Router({
 	mode: 'history',
-	routes: [{
-			path: '/',
-			name: 'HelloWorld',
-			component: HelloWorld
-		},
+	routes: [
 		{
-			path: '/home',
+			path: '/',
 			name: 'Home',
 			component: Home
 		},
