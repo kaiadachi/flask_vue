9d02d0d66e23f8ba0c54fa003d572ec957ea5943
kaiadachi <kaikaikai8217@gmail.com>
2018-12-11 20:24:23 +0900

    change venv

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kai/working/sotuken/backend/venv"
+set -gx VIRTUAL_ENV "/Users/kai/working/sotuken/flaskvue/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
